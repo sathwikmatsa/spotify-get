bbc7b3f263fdfca5c7ca82a5936b6befb721bd4e
Sathwik Matsa <sathwikmatsa@gmail.com>
2019-06-20 22:31:48 +0530

    add config.py
    plug in the credentials

---
+++ spotify-get/config.py
@@ -0,0 +1,13 @@
+# Get spotify credentials at https://developer.spotify.com/dashboard/applications/
+
+SPOTIFY_CLIENT_ID = 'xxx'
+SPOTIFY_CLIENT_SECRET = 'xxx'
+SPOTIFY_REDIRECT_URI = 'xxx'
+
+# Get your username at https://www.spotify.com/is/account/overview/
+
+USERNAME = 'xxx'
+
+# Get Genius access token at http://genius.com/api-clients
+
+GENIUS_CLIENT_ACCESS_TOKEN = 'xxx'
